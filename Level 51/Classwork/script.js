ca634a089576254739fad659abb896ffcdb0d932
GiorgiNarindoshvili <giorgig20099@gmail.com>
2024-11-16 07:20:24 -0500

    commit

---
+++ Level 51/Classwork/script.js
@@ -0,0 +1,6 @@
+//1. Javascript is one of the most popular programing language in the world and based on this proggraming language you can interactive websites. example: You can make some alerts on your website for some customer.
+
+//2.event is action which perfomed by person, for example : Shaking mouse on your screen, This is event, when some pictures or items are moving on our website, That is event too. When you make some value in python or java and you running and it works, Thats also event
+
+//3.
+
